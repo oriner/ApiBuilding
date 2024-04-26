8e061c05bf06cab10f7f9c1ed0192ea9e13a9933
oriner <160720750+oriner@users.noreply.github.com>
2024-04-27 02:25:55 +0300

    Update files with new content

--- django_project/views.py
+++ django_project/views.py
@@ -1,20 +1,21 @@
 import requests
 from django.shortcuts import render 
 
-def index(request):
+def home(request):
   response = requests.get('https://uselessfacts.jsph.pl/random.json?language=en')
   data = response.json()
   fact = data['text']
 
   r3 = requests.get('https://dog.ceo/api/breeds/image/random')
   res3 = r3.json()
-  dog = res3['message']
+  dog = res3['message
 
-  # This is the assignment for the Hackathon, 
-  # Instructions: 
-  # Use this API and randomize the students
-  response2 = requests.get('https://freetestapi.com/api/v1/students') # Use this API
+  response2 = requests.get('https://freetestapi.com/api/v1/students') 
   data2 = response2.json()
   name = data2[0]['name']
+  random.shuffle(students) #Randomly chooses a student name
   
   return render(request, 'templates/index.html', {'fact': fact, 'dog': dog,  'name': name})
+  except Timeouterror as e:
+    error_message = 'Request took too long'
+    return render(request, 'error.html', {'error_message' : error_message})
